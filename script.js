dd4f83436acf37452203d680dfedd5637893470a
EverythingisCopyPastedyay <teemoxdd718@gmail.com>
2026-09-19 20:05:51 +0800

    Updated script.js
    new button and will gonna opens new tab yay

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function showMessage() {
-  // Redirect to Rick Roll video
-  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+function openVideo1() {
+  // Opens YouTube video 1 in a new tab
+  window.open("https://www.youtube.com/watch?v=JwKaHJ6Azs0", "_blank");
+}
+
+function openVideo2() {
+  // Opens YouTube video 2 in a new tab
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
 }
